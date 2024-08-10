117e430db2945913211b311b9ac59a9e3090ea16
Joao Franca <ribeirodefranca23@gmail.com>
2024-08-10 14:31:10 -0300

    feat: add bookinng item

---
+++ app/_components/header.tsx
@@ -0,0 +1,20 @@
+"use client"
+import Image from "next/image"
+import { Card, CardContent } from "./ui/card"
+import { Button } from "./ui/button"
+import { Icon, MenuIcon } from "lucide-react"
+
+const Header = () => {
+  return (
+    <Card>
+      <CardContent className="itens-center flex flex-row justify-between p-5">
+        <Image alt="FSWBarber" src="/logo.png" width={120} height={18} />
+        <Button size="icon" variant="outline">
+          <MenuIcon></MenuIcon>
+        </Button>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default Header
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,69 @@
+import { Input } from "./_components/ui/input"
+import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
+import { SearchIcon } from "lucide-react"
+import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 export default function Home() {
   return (
-    <div className="">
-      <Button>Teste</Button>
+    <div>
+      {/* HEADER */}
+      <Header></Header>
+
+      <div className="p-5">
+        {/* TEXTO */}
+        <h2>Ola, Miguel!</h2>
+        <p className="text-sm">Sexta,2 de fevereito</p>
+
+        {/* BUSCA */}
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon></SearchIcon>
+          </Button>
+        </div>
+
+        {/* IMAGEM */}
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+
+        {/* AGENDAMENTO */}
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Agendamentos
+        </h2>
+        <Card>
+          <CardContent className="flex items-center justify-between p-0">
+            {/* ESQUERDA */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/f64f1bd4-59ce-4ee3-972d-2399937eeafc-16x.png" />
+                </Avatar>
+                <p className="text-sm">FSW Barber</p>
+              </div>
+            </div>
+
+            {/*DIREITA  */}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid p-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">06</p>
+              <p className="text-sm">09:00</p>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   )
 }
