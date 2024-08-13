936d8406292cb243bfef6eefea573ca607b4d24f
Joao Franca <ribeirodefranca23@gmail.com>
2024-08-12 23:00:17 -0300

    feat: add SIdebarSheet component

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -3,6 +3,9 @@ import Image from "next/image"
 import { Card, CardContent } from "./ui/card"
 
 import SidebarSheet from "./sidebar-sheet"
+import { Sheet, SheetTrigger } from "./ui/sheet"
+import { Button } from "./ui/button"
+import { MenuIcon } from "lucide-react"
 
 const Header = async () => {
   return (
@@ -10,7 +13,14 @@ const Header = async () => {
       <CardContent className="itens-center flex flex-row justify-between p-5">
         <Image alt="FSWBarber" src="/logo.png" width={120} height={18} />
 
-        <SidebarSheet />
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button size="icon" variant="outline">
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+          <SidebarSheet />
+        </Sheet>
       </CardContent>
     </Card>
   )
